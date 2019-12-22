a0f8394efc056095654e80864ccba5e5832ae296
esrfree <esrfree@gmail.com>
2019-12-22 18:16:40 -0500

    Customizing HTTP requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,19 @@
 const request = require('request');
 
-const url = 'https://api.darksky.net/forecast/0c4d4a0709ef4e3cfe59706c40748054/37.8267,-122.4233';
+const location = {
+    lat: 25.761681,
+    lon: -80.191788
+}
+const opts = {
+    units: 'si',
+    lang: 'es'
+}
 
-request({ url }, ( error, response ) => {
-    const data = JSON.parse( response.body )
-    console.log(data.currently)
-})
+const url = `https://api.darksky.net/forecast/0c4d4a0709ef4e3cfe59706c40748054/${location.lat},${location.lon}?units=${opts.units}&lang=${opts.lang}`;
+
+request({ url, json: true }, ( error, response ) => {
+    console.log(`${response.body.daily.data[0].summary}
+Actualmente la temperatura es de ${response.body.currently.temperature} grados centigrados. 
+Existe una probabilidad de lluvias del ${response.body.currently.precipProbability} %.`)
+
+});
