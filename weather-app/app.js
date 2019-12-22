10a314135f81b75e56efd92bf059fe6e9fd9dc5a
esrfree <esrfree@gmail.com>
2019-12-22 18:51:09 -0500

    Geolocation API request

--- weather-app/app.js
+++ weather-app/app.js
@@ -9,11 +9,18 @@ const opts = {
     lang: 'es'
 }
 
-const url = `https://api.darksky.net/forecast/0c4d4a0709ef4e3cfe59706c40748054/${location.lat},${location.lon}?units=${opts.units}&lang=${opts.lang}`;
-
-request({ url, json: true }, ( error, response ) => {
-    console.log(`${response.body.daily.data[0].summary}
-Actualmente la temperatura es de ${response.body.currently.temperature} grados centigrados. 
-Existe una probabilidad de lluvias del ${response.body.currently.precipProbability} %.`)
-
+// DarkSky
+const darkskyURL = `https://api.darksky.net/forecast/0c4d4a0709ef4e3cfe59706c40748054/${location.lat},${location.lon}?units=${opts.units}&lang=${opts.lang}`;
+request({ url: darkskyURL, json: true }, ( err, res ) => {
+    console.log(`${res.body.daily.data[0].summary}
+Actualmente la temperatura es de ${res.body.currently.temperature} grados centigrados. 
+Existe una probabilidad de lluvias del ${res.body.currently.precipProbability} %.`)
 });
+
+// Geocoding
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Miami.json?access_token=pk.eyJ1IjoiZXNyZnJlZSIsImEiOiJjazRobXpvY2gxYm9xM2VvNjVnN3g5NXZ5In0.mRSyRiQSdS6UDY0naSB0jw&limit=1';
+request({ url: geocodeURL, json: true }, ( err, res ) => {
+    const latitude = res.body.features[0].center[1];
+    const longitude = res.body.features[0].center[0];
+    console.log( `Latitude: ${latitude}, Longitude: ${longitude}` );
+})
